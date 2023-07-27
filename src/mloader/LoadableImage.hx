13a873952481590b54a5b6c652da2f22d55840ee
littlelingo <clint.little@gmail.com>
2023-07-27 07:51:27 -0400

    Update LoadableImage.hx

--- src/mloader/LoadableImage.hx
+++ src/mloader/LoadableImage.hx
@@ -1,19 +1,3 @@
 package mloader;
 
-#if (js && !(nme || openfl))
-
-	#if haxe3
-	typedef LoadableImage = js.html.ImageElement;
-	#else
-	typedef LoadableImage = js.Dom.Image;
-	#end
-
-#elseif (flash || nme || openfl)
-
-	typedef LoadableImage = flash.display.BitmapData;
-
-#else
-
-	typedef LoadableImage = Dynamic;
-
-#end
+typedef LoadableImage = openfl.display.Loader;
